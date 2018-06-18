816f7c6f999219148fd2c8e70018fbd44cbf1187
karthikuwc <32194038+karthikuwc@users.noreply.github.com>
2018-06-18 22:49:35 +0100

    Add files via upload

--- Get_request.js
+++ Get_request.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
-
-https.get('https://api.iextrading.com/1.0/stock/aapl/chart/5y?filter=date,close,open&chartLast=400', (resp) => {
+function stock_data(ticker) {
+https.get('https://api.iextrading.com/1.0/stock/${ticker}/chart/5y?filter=date,close,open&chartLast=400', (resp) => {
   let data = '';
  
   // A chunk of data has been recieved.
@@ -27,12 +27,24 @@ https.get('https://api.iextrading.com/1.0/stock/aapl/chart/5y?filter=date,close,
     var fs = require('fs');
     fs.writeFile("thing.json", JSON.stringify(array_data));
 
+    console.log(highest_price);
+    console.log(lowest_price);
+    
+    return array_data;
+    
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+}
 
-    var vis = d3.select("#graph")
+
+ var vis = d3.select("#graph")
     .append("svg")
     .attr("width", 500).attr("height", 500);
 
-    
+    var nodes = stock_data("appl");
 
     vis.selectAll("circle.nodes")
     .data(array_data)
@@ -41,14 +53,4 @@ https.get('https://api.iextrading.com/1.0/stock/aapl/chart/5y?filter=date,close,
     .attr("cx", function(d) { return d.open; })
     .attr("cy", function(d) { return d.close; })
     .attr("r", "3px")
-    .attr("fill", "black")
-
-    console.log(highest_price);
-    console.log(lowest_price);
-    console.log(JSON.stringify(array_data));
-    
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
+    .attr("fill", "black");
